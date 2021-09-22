d0055555692c3db3616f12d76914fa1381f4c520
Meruem117 <1572677319@qq.com>
2021-09-22 13:31:27 +0800

    add mysql

--- express-end/app.js
+++ express-end/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const userRouter = require('./routes/user')
 
 const app = express()
 
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/user', userRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ express-end/common/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: "localhost",
+    user: "root",
+    password: "abc123",
+    database: "start"
+}
