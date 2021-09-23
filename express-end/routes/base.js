fd9f8faef24087e12b585fddf253079ea8164763
Meruem117 <1572677319@qq.com>
2021-09-23 23:23:51 +0800

    change controller to service

--- express-end/routes/base.js
+++ express-end/routes/base.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const con = require('../controllers/base')
+const con = require('../services/base')
 
 router.get('/', async (req, res, next) => {
     const data = await con.showDatabases()
