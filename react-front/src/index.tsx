c34155f174d203c4039f22316a817cd797958013
Meruem117 <1572677319@qq.com>
2022-11-11 23:43:55 +0800

    udpate react

--- react-front/src/index.tsx
+++ react-front/src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import Home from './views/Home'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ react-front/src/views/Home.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+
+export default function Home() {
+  return (
+    <Fragment>
+      <div>Home</div>
+    </Fragment>
+  )
+}
