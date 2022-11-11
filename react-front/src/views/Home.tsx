f18d0cb601382fd8544fbf26634f58c4fdea0e28
Meruem117 <1572677319@qq.com>
2022-11-12 00:34:52 +0800

    add select

--- react-front/src/views/Home.tsx
+++ react-front/src/views/Home.tsx
@@ -1,15 +1,61 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Box, Typography } from '@mui/material'
+import { FormControl, InputLabel, Select, MenuItem, FormHelperText, SelectChangeEvent } from '@mui/material'
 import Nav from '../components/Nav'
 
 export default function Home(): React.ReactElement {
+  const [database, setDatabase] = useState<string>('')
+  const [table, setTable] = useState<string>('')
+
+  const changeDatabase = (event: SelectChangeEvent): void => {
+    setDatabase(event.target.value)
+  }
+
+  const changeTable = (event: SelectChangeEvent): void => {
+    setTable(event.target.value)
+  }
+
   return (
     <Fragment>
       <Nav active='1' />
-      <Box sx={{ textAlign: 'center' }} mt={2}>
+      <Box sx={{ textAlign: 'center' }} mt={2} mb={2}>
         <Typography variant="h4">
           Home
         </Typography>
+      </Box>
+      <Box display={'flex'} justifyContent={'center'}>
+        <FormControl sx={{ m: 1, minWidth: 360 }}>
+          <InputLabel>Database</InputLabel>
+          <Select
+            value={database}
+            label="Database"
+            onChange={changeDatabase}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+          <FormHelperText>Select a database</FormHelperText>
+        </FormControl>
+        <FormControl sx={{ m: 1, minWidth: 360, marginLeft: 5 }}>
+          <InputLabel>Table</InputLabel>
+          <Select
+            value={table}
+            label="Table"
+            onChange={changeTable}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+          <FormHelperText>Select a table</FormHelperText>
+        </FormControl>
       </Box>
     </Fragment>
   )
